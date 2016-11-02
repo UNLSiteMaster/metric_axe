ec28f8cf7ee9fd555938615150048292f37299a0
Michael Fairchild <mfairchild365@gmail.com>
2016-11-02 15:29:36 -0500

    code styels

--- www/templates/html/Metric.tpl.php
+++ www/templates/html/Metric.tpl.php
@@ -4,11 +4,11 @@
 <p>
     <?php $widget_url = \SiteMaster\Core\Config::get('URL') . 'plugins/metric_axe/widget.js.php'; ?>
     This metric tests your pages against the WCAG 2.0 level AA standard. To locate and fix errors on your page, drag this link to your browser bookmarks bar:
-    <a class="wdn-button" href="javascript: (function () { 
+    <a class="wdn-button" href="javascript: (function () {
         var jsCode = document.createElement('script'); 
-        jsCode.setAttribute('src', '<?php echo $widget_url ?>');                  
+        jsCode.setAttribute('src', '<?php echo $widget_url ?>');
         document.body.appendChild(jsCode); 
-     }());" onclick="alert('Drag this to your browser Bookmarks Bar to install.'); return false;">SiteMaster Axe</a>, then click the 'SiteMaster aXe' bookmark to run accessibility tests on any page. Note that pages are checked at a mobile width.
+     }());" onclick="alert('Drag this to your browser Bookmarks Bar to install.'); return false;">SiteMaster Axe</a>, then click the 'SiteMaster aXe' bookmark to run accessibility tests on any page. <strong>The bookmarklet will log information to the JavaScript Console</strong>. For best results, run the bookmarklet at a mobile width.
 </p>
 <?php
 $included_file = __DIR__.'/../../../custom-message.html';
