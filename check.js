9b9fec6d5f4cfa8628ccf1a7389e0b0a496edfcb
Jeff Sturek <jsturek@gmail.com>
2020-12-01 10:48:13 -0600

    Support darkmode scans with puppeteer (#2)
    * Support darkmode scans with puppeteer
    
    * Add check.js
    
    * Update axe-core to 4.1.0
    
    Co-authored-by: Jeff Sturek <jsturek8@unl.edu>

---
+++ check.js
@@ -0,0 +1,68 @@
+const argv = require('minimist')(process.argv.slice(2));
+const { URL } = require('url');
+//Convert to an absolute URL by default (for example, this will add a trailing slash after the domain if it wasn't provided)
+const url = new URL(argv._[0]).href;
+
+const util = require('util');
+const puppeteer = require('puppeteer');
+const axe = require('axe-core');
+const fs = require('fs');
+let path = __dirname + '/config/axe-options.inc.json';
+if (!fs.existsSync(path)) {
+  path = __dirname + '/config/axe-options.sample.json';
+}
+const axeOptions = JSON.parse(fs.readFileSync(path, 'utf8'));
+
+const getViolations = async (url, darkMode = false) => {
+  try {
+    let options = {headless: true};
+    if (argv.sandbox === 'false') {
+      options.args = ['--no-sandbox', '--disable-setuid-sandbox'];
+    }
+    const prefersColorScheme = darkMode === true ? 'dark' : 'light';
+    const browser = await puppeteer.launch(options);
+    const page = await browser.newPage();
+
+    if (argv.ua) {
+      await page.setUserAgent(argv.ua);
+    }
+
+    await page.on('dialog', async dialog => {
+      //Auto dismiss dialogs so that the process does not hang waiting on user input.
+      await dialog.dismiss();
+    });
+
+    await page.emulateMediaFeatures([{name: 'prefers-color-scheme', value: prefersColorScheme}]);
+    await page.goto(url);
+    await page.addScriptTag({
+      path: require.resolve('axe-core')
+    });
+
+    // run axe on the page
+    const axeResults = await page.evaluate(async (axeOptions) => {
+      return await axe.run(axeOptions);
+    }, axeOptions);
+
+    await browser.close();
+
+    return axeResults.violations;
+  } catch (e) {
+    //fail early
+    process.exit(1);
+  }
+};
+
+(async () => {
+  const violations = await getViolations(url, false);
+  const darkModeContrastViolations = argv.dark_mode === 'true' ? await getViolations(url, true) : [];
+  console.log(
+    JSON.stringify(
+      violations.concat(
+        darkModeContrastViolations.filter(violation => violation.id === 'color-contrast')
+          .map((item) => {
+            item.id += '-darkmode';
+            item.help += ' (Dark Mode)';
+            return item;
+          })
+      )));
+})();
--- readme.md
+++ readme.md
@@ -5,29 +5,25 @@ This is an automated accessibility testing metric powered via [aXe core](https:/
 It also provides a bookmarklet for live testing. Normally aXe testing is accomplished via browser extensions, however because the browser extensions can be updated to a different version of the aXe core library than the version that SiteMaster is using, a bookmarklet is an easy way to ensure that the same exact tests are done live as they are done by SiteMaster.
 
 ## Install
-To install follow these steps
+To install follow these steps (note: requires node.js >= v15)
 
 1. clone this repo to sitemaster's `plugins/metric_axe` directory
-2. install `axe-core` with npm in the `plugins/metric_axe` directory: `npm install axe-core`
+2. install `axe-core` with npm in the `plugins/metric_axe` directory: `npm install axe-core` Note: also requires puppeteer minimist which should be install with site_master base
 3. update sitemaster's config to include the `metric_axe` plugin
 4. if you want to customize the testing config, copy `plugins/metric_axe/config/axe-options.sample.json` to `plugins/metric_axe/config/axe-options.inc.json` and customize
 5. Add the following to the `Config::set('PLUGINS', ` array and customize as needed. This will make SiteMaster aware of the plugin but not apply it to any groups.
 ```
 'metric_axe' => [
   // 'weight' => 20, // Adjust weight if desired
+  // 'execute_as_user' => 'webaudit-chrome',
+  // 'sandbox' => false,
+  // 'dark_mode' => false  // Whether to check for darkmode color constrast violations
 ],
 ```
 6. Add the following your group configuration under the `METRICS` key. This will enable it for the specified group. All metric configuration will override the defaults set in the `'PLUGINS'` array.
 ```
 'metric_axe' => [
   // 'weight' => 20, // Adjust weight if desired
+  // 'dark_mode' => false  // Whether to check for darkmode color constrast violations
 ],
-```
-
-## Testing
-
-from the sitemaster root directory, run
-
-```
-./vendor/bin/phpunit plugins/metric_axe/tests
 ```
