0e95378e2d2bf8857294ac0767cb9673e16ea269
Eric Rasmussen <ericrasmussen1@gmail.com>
2022-08-01 09:07:18 -0500

    Update Metric.tpl.php

--- www/templates/html/Metric.tpl.php
+++ www/templates/html/Metric.tpl.php
@@ -1,5 +1,5 @@
 <p>
-    aXe can't catch all accessibility errors. It'll catch many of them, but <strong>you must do manual checking as well</strong>. Automated accessibility testing tools can only test about 40% of accessibility problems.
+    Axe can't catch all accessibility errors. It'll catch many of them, but <strong>you must do manual checking as well</strong>. Automated accessibility testing tools can only test about 40% of accessibility problems.
 </p>
 <p>
     <?php $widget_url = \SiteMaster\Core\Config::get('URL') . 'plugins/metric_axe/widget.js.php'; ?>
@@ -8,7 +8,7 @@
         var jsCode = document.createElement('script'); 
         jsCode.setAttribute('src', '<?php echo $widget_url ?>');
         document.body.appendChild(jsCode); 
-     }());" onclick="alert('Drag this to your browser Bookmarks Bar to install.'); return false;">SiteMaster Axe</a>, then click the 'SiteMaster aXe' bookmark to run accessibility tests on any page. <strong>The bookmarklet will log information to the JavaScript Console</strong>. For best results, run the bookmarklet at a mobile width.
+     }());" onclick="alert('Drag this to your browser Bookmarks Bar to install.'); return false;">SiteMaster Axe</a>, then click the 'SiteMaster Axe' bookmark to run accessibility tests on any page. <strong>The bookmarklet will log information to the JavaScript Console</strong>. For best results, run the bookmarklet at a mobile width.
 </p>
 <?php
 $included_file = __DIR__.'/../../../custom-message.html';
@@ -18,5 +18,5 @@ if (file_exists($included_file)) {
 
 ?>
 <p>
-    This service is provided by <a href="https://github.com/dequelabs/axe-core">aXe core</a>
+    This service is provided by <a href="https://github.com/dequelabs/axe-core">axe core</a>
 </p>
